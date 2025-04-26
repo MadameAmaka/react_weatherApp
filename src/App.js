ddd7cef6337c236b2d374769e7f5e8b6bde905e6
Amaka Onwuli <amonwuli@gmail.com>
2025-04-26 22:39:16 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
     <div>
-     <Weather defaultCity="New York" />
+     <Weather/>
      <footer> This project was coded by <a href='https://github.com/MadameAmaka' target='_blank' rel="noreferrer"> Amaka Onwuli </a> and is open-sourced <a href='https://github.com/MadameAmaka/react_weather_project' target='_blank' rel="noreferrer" > on Github,
     </a> and hosted on <a href='https://amakas-react-weather-project.netlify.app/' target='_blank' rel="noreferrer"> Netlify. </a></footer>
     </div></div>
---
+++ src/Weather.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export default function Weather(props){
+    return "hello from weather"
+}
